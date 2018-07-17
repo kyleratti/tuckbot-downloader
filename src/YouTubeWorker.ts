9500929ba06b6b01f5c6445114ead1b79584da5e
Kyle <kyle.ratti@outlook.com>
2018-07-16 22:29:40 -0400

    Updated worker to store in OS temp folder

--- src/YouTubeWorker.ts
+++ src/YouTubeWorker.ts
@@ -8,29 +8,34 @@ export interface ConfigOptions {
     video: Video,
     /** The full path to the temporary folder for this video file */
     tempFolder: string,
+    /** The file name with extension */
+    fileName: string,
 }
 
 export class YouTubeWorker {
     /** The Video object this worker is attached to */
     video: Video;
-    /** The full path to the temporary location of the video file */
-    tempLocation: string;
+    /** The full path to the temporary folder of the video file */
+    tempFolder: string;
+    /** The file name with extension */
+    fileName: string;
 
     constructor(options:ConfigOptions) {
         this.video = options.video;
-        this.tempLocation = options.tempFolder;
+        this.tempFolder = options.tempFolder;
+        this.fileName = options.fileName;
     }
 
     /**
      * Cleans up any left over temp files
      */
     cleanup() {
-        fs.exists(this.tempLocation, (exists) => {
-            if(exists) {
-                fs.unlink(this.tempLocation, (e) => {
+        let filePath = path.resolve(this.tempFolder, this.fileName);
+        fs.exists(filePath, (exists) => {
+            if(exists)
+                fs.unlink(filePath, (e) => {
                     console.error("Can't remove temp file: %s", e);
                 });
-            }
         });
     }
 
@@ -39,16 +44,17 @@ export class YouTubeWorker {
      */
     start() {
         return new Promise((success, fail) => {
-            let strBaseDir = path.resolve('../', this.tempLocation);
-            this.tempLocation = path.resolve(strBaseDir, (this.video.redditPostId + '.mp4'));
+            if(!fs.existsSync(this.tempFolder))
+                fs.mkdirSync(this.tempFolder);
+            let filePath = path.resolve(this.tempFolder, this.fileName);
 
             ytdl(this.video.post.url, {filter: (format) => format.container === 'mp4'})
                 .on('error', (e) => {
-                    fail(e);
-
                     this.cleanup();
+
+                    fail(e);
                 })
-                .pipe(fs.createWriteStream(this.tempLocation))
+                .pipe(fs.createWriteStream(filePath))
                 .on('finish', () => {
                     success();
                 });
