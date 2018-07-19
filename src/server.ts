1ead1b2a5fa8f737d768a3cf3967bcad7edefa21
Kyle <kyleratti@users.noreply.github.com>
2018-07-18 21:57:02 -0400

    Removed old comments

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,6 @@ import { MirrorRequestScanner, ReplyScanner } from './scanners';
 
 export default class {
     start() {
-        /**
-         * Start scanner that checks for new mirror requests
-         * Start scanner that checks for mirrors that are done and waiting for replies to be posted
-         */
         let mirrorReqScanner = new MirrorRequestScanner({
             scanInterval: 1000 * 10
         });
