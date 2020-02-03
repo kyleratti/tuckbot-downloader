081de116a018be227f19ac8e5623778cc99aa2ec
Kyle Ratti <kyleratti@users.noreply.github.com>
2020-02-02 21:55:21 -0500

    Cleaned up import

--- src/downloaders/videodownloader.ts
+++ src/downloaders/videodownloader.ts
@@ -2,7 +2,7 @@ import ffmpeg_bin from "ffmpeg-static";
 import Ffmpeg from "fluent-ffmpeg";
 import fs from "fs";
 import glob from "glob";
-import * as path from "path";
+import path from "path";
 import { resolve } from "path";
 import { configurator } from "tuckbot-util";
 import youtubedl from "youtube-dl";
