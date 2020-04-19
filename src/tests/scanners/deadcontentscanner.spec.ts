d67a702bdbbb2b49fdc0fe59228721ff75d74097
Kyle Ratti <kyleratti@users.noreply.github.com>
2020-04-19 13:12:48 -0400

    Create deadcontentscanner.spec.ts

---
+++ src/tests/scanners/deadcontentscanner.spec.ts
@@ -0,0 +1,28 @@
+import { TuckbotApi } from "../../services";
+import { Submission } from "snoowrap";
+import { snooman } from "tuckbot-util";
+
+describe("Dead Content Scanner", () => {
+  test("retrieve dead content from tuckbot api", async () => {
+    return expect(async () => {
+      (await TuckbotApi.fetchStale()).data.staleVideos;
+    }).not.toThrow();
+  });
+
+  test("retrieve reddit post info for dead content", async () => {
+    let videos = (await TuckbotApi.fetchStale()).data.staleVideos;
+    let allVidsLoaded = true;
+
+    videos.forEach(async (vid) => {
+      try {
+        // @ts-ignore
+        await snooman.wrap.getSubmission(vid.redditPostId).fetch();
+      } catch (err) {
+        console.error("ERRR");
+        allVidsLoaded = false;
+      }
+    });
+
+    return expect(allVidsLoaded).toBeTruthy();
+  });
+});
