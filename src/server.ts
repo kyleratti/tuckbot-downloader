8135494716f949b88139a2d4c7b2501a995c946c
Kyle <kyleratti@users.noreply.github.com>
2020-06-15 17:36:07 -0400

    Add TODO to scanner intervals

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,13 @@ import { DeadContentScanner, SubredditScanner } from "./scanners";
 
 export class Server {
   start() {
-    let subredditScanner = new SubredditScanner({
-      scanInterval: 1000 * 20,
+    const subredditScanner = new SubredditScanner({
+      scanInterval: 1000 * 20, // TODO: set these from an environment variable
     });
     subredditScanner.start();
 
-    let deadcontentScanner = new DeadContentScanner({
-      scanInterval: 1000 * 30,
+    const deadcontentScanner = new DeadContentScanner({
+      scanInterval: 1000 * 30, // TODO: set these from an environment variable
     });
     deadcontentScanner.start();
   }
